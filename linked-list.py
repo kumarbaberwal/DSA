e975d4cb25a7ff0527ee52df65467e9603497e9b
kumarbaberval <niteshkumarghn1@gmail.com>
2024-02-25 18:39:57 +0530

    Linked List Add

--- linked-list.py
+++ linked-list.py
@@ -23,9 +23,32 @@ class LinkedList:
             print(pointer.data)
             pointer=pointer.pointer
 
+    def InsertAtIndex(self,data,index):
+        newNode=Node(data)
+        pointer=self.head
+        i=0
+        while i<index:
+            temp=pointer
+            pointer=pointer.pointer
+            i=i+1
+        temp.pointer=newNode
+        newNode.pointer=pointer
+
+    def InsertAtEnd(self,data):
+        newNode=Node(data)
+        pointer=self.head
+        while pointer:
+            temp=pointer
+            pointer=pointer.pointer
+        temp.pointer=newNode
+
+
 
 linkedlist=LinkedList()
 
 linkedlist.InsertAtBegining(5)
 linkedlist.InsertAtBegining(9)
+linkedlist.InsertAtBegining(10)
+linkedlist.InsertAtIndex(13,1)
+linkedlist.InsertAtEnd(40)
 linkedlist.display()
