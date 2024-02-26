136f04f3e5704b0722cf95631abce9d4c2debe13
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-02-26 22:51:26 +0530

    Circular Linked List

--- circular-linkedlist.py
+++ circular-linkedlist.py
@@ -7,31 +7,59 @@ class CircularLinkedList:
     def __init__(self) -> None:
         self.head=None
 
-    def InsertAtStart(self,data):
+    def InsertAtBegin(self,data):
         newNode=Node(data)
-        newNode.pointer=self.head
         if self.head==None:
             self.head=newNode
             newNode.pointer=self.head
             return
+
         else:
+            pointer=self.head.pointer
+            while pointer!=self.head:
+                temp=pointer
+                pointer=pointer.pointer
             newNode.pointer=self.head
             self.head=newNode
+            temp.pointer=self.head
 
-    # def InsertAtEnd(self,data):
-
+    def InsertAtEnd(self,data):
+        newNode=Node(data)
+        if self.head==None:
+            self.head=newNode
+            newNode.pointer=self.head
+            return
+        
+        else:
+            pointer=self.head
+            if pointer.pointer==self.head:
+                pointer.pointer=newNode
+                newNode.pointer=self.head
+                return
+            
+            else:
+                pointer=self.head.pointer
+                while pointer!=self.head:
+                    temp=pointer
+                    pointer=pointer.pointer
+                temp.pointer=newNode
+                newNode.pointer=self.head
 
     def display(self):
-        pointer=self.head
-        print(pointer.data)
-        pointer=pointer.pointer
+        pointer=self.head.pointer
+        print(self.head.data)
         while pointer!=self.head:
             print(pointer.data)
             pointer=pointer.pointer
 
 
+
 circular=CircularLinkedList()
-circular.InsertAtStart(4)
-circular.InsertAtStart(5)
-circular.InsertAtStart(6)
+circular.InsertAtEnd(4)
+circular.InsertAtEnd(5)
+circular.InsertAtEnd(6)
+
+circular.InsertAtBegin(1)
+circular.InsertAtBegin(2)
+
 circular.display()
