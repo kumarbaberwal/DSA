203de2d0d1bf955747aa1fab7dca057d11fcd37c
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-03-02 14:48:03 +0530

    Binary Tree Updated

--- binary-tree.py
+++ binary-tree.py
@@ -37,10 +37,31 @@ class BinaryTree:
                 self.insertrecursively(node.right, new_node)
 
     def display(self, node):
+        root=self.root
+        if root is None:
+            print('Binary Tree is Empty.')
         if node is not None:
             self.display(node.left)
             print(node.data)
             self.display(node.right)
+
+    def minimum(self):
+        node=self.root
+        if node is None:
+            print('Tree is Empty.')
+        else:
+            while node.left is not None:
+                node=node.left
+            print(node.data)
+
+    def maximum(self):
+        root=self.root
+        if root is None:
+            print('Tree is Empty.')
+        else:
+            while root.right is not None:
+                root=root.right
+            print(root.data)
 
 bt = BinaryTree()
 bt.insert(4)
@@ -50,3 +71,9 @@ bt.insert(7)
 bt.insert(1)
 
 bt.display(bt.root)
+print()
+print('Minimun Element: ',end=' ')
+bt.minimum()
+print()
+print('Maximum Element: ',end=' ')
+bt.maximum()
