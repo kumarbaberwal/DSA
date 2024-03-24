2c18e6eeae2d26e82a88816b263889f6141388b2
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-03-24 23:52:00 +0530

    maxheap.py

--- maxheap.py
+++ maxheap.py
@@ -1,1 +1,23 @@
 arr=[3,9,2,4,10,1,5,6,11]
+
+def heapify(arr,n,i):
+    largest=i
+    left=2*i+1
+    right=2*i+2
+
+    if left<n and arr[left]>arr[largest]:
+        largest=left
+    if right<n and arr[right]>arr[largest]:
+        largest=right
+
+    if largest!=i:
+        arr[largest],arr[i]=arr[i],arr[largest]
+        heapify(arr,n,largest)
+
+
+n=len(arr)
+for i in range((n//2)-1,-1,-1):
+    heapify(arr,n,i)
+
+
+print(arr)
