36779e19c51b0b37b3af963634daea451aca3e80
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-03-28 23:00:15 +0530

    test.py

--- test.py
+++ test.py
@@ -1,52 +1,26 @@
-class TreeNode:
-    def __init__(self, data) -> None:
-        self.data=data
-        self.left=None
-        self.right=None
+arr=[8,1,9,2,4,3,5,7,6]
 
-class BinaryTree:
-    def __init__(self) -> None:
-        self.root=None
+def partition(arr,start,end):
+    i=start
+    j=end
+    pivot=arr[start]
+    while (i<j):
+        while arr[i]<=pivot:
+            i+=1
+        while arr[j]>pivot:
+            j-=1
+        
+        if i<j:
+            arr[i],arr[j]=arr[j],arr[i]
+    arr[start],arr[j]=arr[j],arr[start]
+    return j
 
-    def insert(self, data):
-        if self.root is None:
-            self.root=TreeNode(data)
-            return
-        else: 
-            self.recursivelyinsert(self.root,TreeNode(data))
-                
-    def recursivelyinsert(self, node, newnode):
-        if newnode.data<node.data:
-            if node.left is None:
-                node.left=newnode
-                return
-            else:
-                self.recursivelyinsert(node.left,newnode)
-        else:
-            if node.right is None:
-                node.right=newnode
-                return
-            else:
-                self.recursivelyinsert(node.right,newnode)
+def quicksort(arr,start,end):
+    if start<end:
+        index=partition(arr,start,end)
+        quicksort(arr,start,index-1)
+        quicksort(arr,index+1,end)
 
+quicksort(arr,0,len(arr)-1)
 
-    def inorder(self):
-        self._inorder(self.root)
-
-    def _inorder(self,node):
-        root=self.root
-        if root is None:
-            print("Binary Tree is Empty.")
-            return
-        else:
-            if node is not None:
-                self._inorder(node.left)
-                print(node.data)
-                self._inorder(node.right)
-
-bt=BinaryTree()
-bt.insert(7)
-bt.insert(4)
-bt.insert(1)
-bt.insert(9)
-bt.inorder()
+print(arr)
