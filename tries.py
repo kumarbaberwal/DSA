612104bca31efcc5b0f8ddca7b3ad914eced0766
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-04-01 21:16:16 +0530

    tries.py

---
+++ tries.py
@@ -0,0 +1,10 @@
+class TriesNode:
+    def __init__(self,data) -> None:
+        self.data=data
+        self.right=None
+        self.left=None
+
+class Tries:
+    def __init__(self) -> None:
+        self.root=None
+
