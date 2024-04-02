b9d70b3bfb89b86cf24e7df703c2295ca5965dae
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-04-02 23:46:02 +0530

    Trie Node

--- tries.py
+++ tries.py
@@ -1,10 +1,16 @@
-class TriesNode:
-    def __init__(self,data) -> None:
-        self.data=data
-        self.right=None
-        self.left=None
+class TrieNode:
+    def __init__(self) -> None:
+        self.children=[None]*26
+        self.isEndOfWord=False
+        
 
 class Tries:
     def __init__(self) -> None:
-        self.root=None
+        self.root=self.getNode()
+        print('Root',self.root)
 
+    def getNode(self):
+        return TrieNode()
+
+
+Trie=Tries()
