77b7706a0f6449e5a5ac52dfaf079120a994229a
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-04-03 01:55:13 +0530

    Trie insertion and search

--- tries.py
+++ tries.py
@@ -6,11 +6,46 @@ class TrieNode:
 
 class Tries:
     def __init__(self) -> None:
-        self.root=self.getNode()
-        print('Root',self.root)
+        self.root=TrieNode()
+        # print('Root',self.root)
+    
+    def _charToIndex(self,char):
+        return ord(char)-ord('a')
+    
+    def insert(self,character):
+        node=self.root
+        length=len(character)
+        for i in range(length):
+            index=self._charToIndex(character[i])
+            if not node.children[index]:
+                node.children[index]=TrieNode()
+            node=node.children[index]
+        node.isEndOfWord=True
 
-    def getNode(self):
-        return TrieNode()
+    def search(self,character):
+        root=self.root
+        lenght=len(character)
+        for i in range(lenght):
+            index=self._charToIndex(character[i])   
+            if not root.children[index]:
+                return False
+            root=root.children[index]
+        return root.isEndOfWord
 
+    def display(self,character):
+        val=self.search(character)
+        if val is True:
+            print('Present in Trie.') 
+        else:
+            print('Not Present in Trie.')
 
 Trie=Tries()
+characters=['arun','kumar','shivam','shreya']
+for i in characters:
+    Trie.insert(i)
+
+print(Trie.search('kumar'))
+print(Trie.search('kunal'))
+Trie.display('kumar')
+Trie.display('shreya')
+Trie.display('kunal')
