30c6062dad20a0aebdff5e4558931b397e201524
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-04-07 00:15:41 +0530

    graph.py

--- graph.py
+++ graph.py
@@ -7,9 +7,32 @@
 
 # Graphs using adjacency list 
 
-def edge(graph, src, dest):
+def addedge(graph, src, dest):
     graph[src].append(dest)
 
 v=4
 
-def graph
+def graph(v, edges, noOfEdges):
+    graph=[0]*v
+
+    for i in range(len(graph)):
+        graph[i]=[]
+
+    for i in range(noOfEdges):
+        addedge(graph,edges[i][0],edges[i][1])
+
+    adjList(graph,v)
+
+def adjList(graph,v):
+    for i in range(v):
+        print(i,' -> ',end='')
+        for x in graph[i]:
+            print(x, " ",end='')
+
+        print()
+
+edges=[[0,2],[1,2],[1,3],[2,0],[2,1],[2,3],[3,1],[3,2]]
+
+noOfEdges=8
+
+graph(v,edges,noOfEdges)
