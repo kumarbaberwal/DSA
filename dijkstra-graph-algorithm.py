b04aa4877f0225d9604c0f81a6f2ca3ae9420588
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-04-19 00:45:04 +0530

    dijkstra-graph-algorithm.py

---
+++ dijkstra-graph-algorithm.py
@@ -0,0 +1,33 @@
+edges=[[0,1,2],[0,2,4],[1,3,7],[1,2,1],[2,4,3],[3,5,1],[4,5,5]]
+noOfEdges=len(edges)
+
+def addEdge(adj, src, dest, weight):
+    adj[src].append([dest,weight])
+    adj[dest].append([src,weight])
+    return adj
+
+def printGraph(adj , v):
+
+    for i in range(v):
+        print('Node',i , 'makes an edge with')
+
+        for j in adj[i]:
+            print('\tNode', j[0], 'with edge weight = ', j[1])
+        print()
+
+v=6
+adj=[[] for i in range(v)]
+
+
+
+# def dijkstra(adj:list[list[int]], src:int):
+
+
+def graph(v, edges, noOfEdges):
+
+    for i in range(noOfEdges):
+        addEdge(adj,edges[i][0],edges[i][1],edges[i][2])
+    
+
+graph(v, edges, noOfEdges)
+printGraph(adj,v)
