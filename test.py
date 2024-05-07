acef49d88398e0280323c2641d36874e3ffe304f
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-05-07 11:45:49 +0530

    sessional question

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 def addEdge(adj: list[list[int]], src: int, dest: int, weight: int) -> None:
     adj[src].append([dest,weight])
+    # print(adj)
 
 def dijkstra(adj: list[list[int]], src: int, vertices: int) -> None:
     distances = [float('inf')] * vertices
@@ -7,11 +8,13 @@ def dijkstra(adj: list[list[int]], src: int, vertices: int) -> None:
     import heapq
     queue = [(0, src)]
     while queue:
+        # print(distances)
         current_weight, current_node = heapq.heappop(queue)
         for neighbour, weight in adj[current_node]:
             if distances[current_node] + weight < distances[neighbour]:
                 distances[neighbour] = current_weight + weight
                 heapq.heappush(queue,(distances[neighbour],neighbour))
+                print(queue)
 
     return distances
 
@@ -23,7 +26,15 @@ def graph(vertices: int, edges: list[list[int]], noOfEdges: int) -> None:
     print("Shortest Distance from the node 0: ", dijkstra(adj, 0, vertices))
 
 if __name__ == "__main__" :
-    vertices = 6
-    edges = [[0,1,2],[0,2,4],[1,0,2],[1,2,1],[1,3,7],[2,0,4],[2,1,1],[2,4,3],[3,1,7],[3,4,2],[3,5,1],[4,2,3],[4,3,2],[4,5,5],[5,3,1],[5,4,5]]
+    vertices = 9
+    edges = [[0,1,4],[0,4,3],
+             [1,0,4],[1,2,8],[1,4,11],
+             [2,1,8],[2,3,7],[2,6,4],[2,8,2],
+             [3,2,7],[3,6,14],[3,7,9],
+             [4,0,3],[4,1,11],[4,5,1],[4,8,7],
+             [5,4,1],[5,6,2],[5,8,6],
+             [6,2,4],[6,3,14],[6,5,2],[6,7,10],
+             [7,3,9],[7,6,10],
+             [8,2,2],[8,4,7],[8,5,6]]
     noOfEdges = len(edges)
     graph(vertices, edges, noOfEdges)
