778c7ea7c3f700c190d603ac8ed7f101e4c58b34
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-05-07 21:32:36 +0530

    sessional question 3

--- test.py
+++ test.py
@@ -1,43 +1,40 @@
-def addEdge(adj: list[list[list[int]]], src: int, dest: int, weight: int) -> None:
-    adj[src].append([dest, weight])
-    # print(adj)
+def addEdge(adj: list[list[int]], src: int, dest: int, weight: int) -> None:
+    adj[src].append([dest,weight])
+
+def dijkstra(adj: list[list[int]], src: int, vertices: int) -> None:
+    distances = [float('inf')] * vertices
+    distances[src] = 0
+    import heapq
+    queue = [(0, src)]
+    while queue:
+        current_weight, current_node = heapq.heappop(queue)
+        for neighbour, weight in adj[current_node]:
+            if distances[current_node] + weight < distances[neighbour]:
+                distances[neighbour] = current_weight + weight
+                heapq.heappush(queue,(distances[neighbour],neighbour))
+                print(queue)
+
+    return distances
 
 def graph(vertices: int, edges: list[list[int]], noOfEdges: int) -> None:
     adj = [[] for _ in range(vertices)]
     for i in range(noOfEdges):
         addEdge(adj, edges[i][0], edges[i][1], edges[i][2])
+    # print(adj)
+    print("Shortest Distance from the node 0: ", dijkstra(adj, 0, vertices))
 
-    prims(adj, vertices)
-
-def prims(adj:list[list[list[int]]], vertices:int) -> None:
-    import heapq
-    queue=[(0,0)]
-    vis=[False]*vertices
-    mstCost=0
-    while queue:
-        current_weight, current_node = heapq.heappop(queue)
-        if not vis[current_node]:
-            vis[current_node]=True
-            mstCost += current_weight
-
-            for i in adj[current_node]:
-                weight = i[1]
-                if not vis[i[0]]:
-                    heapq.heappush(queue, (weight, i[0]))    
-    print()
-    print("Minimum Cost of MST :- ", mstCost)
-    
-
-if __name__ == "__main__":
+if __name__ == "__main__" :
     vertices = 9
-    edges = [[0,1,7],[0,8,15],
-             [1,0,7],[1,2,5],
-             [2,1,5],[2,3,4],[2,4,2],[2,6,3],
-             [3,2,4],[3,4,1],[3,5,9],
-             [4,2,2],[4,3,1],
-             [5,3,9],[3,7,12],[5,6,5],
-             [6,2,3],[6,5,5],[6,8,6],
-             [7,5,12],[7,8,10],
-             [8,0,15],[8,6,6],[8,7,10]]
+    edges = [
+        [0,1,4],[0,4,8],
+        [1,0,4],[1,2,8],[1,4,11],
+        [2,1,8],[2,3,7],[2,8,2],
+        [3,2,7],[3,6,14],[3,7,9],
+        [4,0,8],[4,1,11],[4,5,1],[4,8,7],
+        [5,4,1],[5,6,2],[5,8,6],
+        [6,2,4],[6,5,2],[6,7,10],
+        [7,3,9],[7,6,10],
+        [8,2,2],[8,4,7],[8,5,6]
+    ]
     noOfEdges = len(edges)
     graph(vertices, edges, noOfEdges)
