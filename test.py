6c375e5244262899fe3a4bf61d486d3d86331e5e
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-05-07 13:40:20 +0530

    test

--- test.py
+++ test.py
@@ -2,39 +2,35 @@ def addEdge(adj: list[list[int]], src: int, dest: int, weight: int) -> None:
     adj[src].append([dest,weight])
     # print(adj)
 
-def dijkstra(adj: list[list[int]], src: int, vertices: int) -> None:
-    distances = [float('inf')] * vertices
-    distances[src] = 0
-    import heapq
-    queue = [(0, src)]
-    while queue:
-        # print(distances)
-        current_weight, current_node = heapq.heappop(queue)
-        for neighbour, weight in adj[current_node]:
-            if distances[current_node] + weight < distances[neighbour]:
-                distances[neighbour] = current_weight + weight
-                heapq.heappush(queue,(distances[neighbour],neighbour))
-                print(queue)
+def distances(adj: list[list[int]], vertices: int) -> None:
+    distance=[[float('inf') for _ in range(vertices)] for _ in range(vertices)]
+    # Set initial distances based on direct edges
+    for i in range(vertices):
+        for j, weight in adj[i]:
+            distance[i][j] = weight
+    
+    # Set distance to self as 0
+    for i in range(vertices):
+        distance[i][i] = 0
 
-    return distances
+    for k in range(vertices):
+        for i in range(vertices):
+            for j in range(vertices):
+                distance[i][j]=min(distance[i][j],distance[i][k] + distance[k][j])
+
+    print(distance)
 
 def graph(vertices: int, edges: list[list[int]], noOfEdges: int) -> None:
     adj = [[] for _ in range(vertices)]
     for i in range(noOfEdges):
         addEdge(adj, edges[i][0], edges[i][1], edges[i][2])
-    # print(adj)
-    print("Shortest Distance from the node 0: ", dijkstra(adj, 0, vertices))
+    distances(adj, vertices)
 
 if __name__ == "__main__" :
-    vertices = 9
-    edges = [[0,1,4],[0,4,3],
-             [1,0,4],[1,2,8],[1,4,11],
-             [2,1,8],[2,3,7],[2,6,4],[2,8,2],
-             [3,2,7],[3,6,14],[3,7,9],
-             [4,0,3],[4,1,11],[4,5,1],[4,8,7],
-             [5,4,1],[5,6,2],[5,8,6],
-             [6,2,4],[6,3,14],[6,5,2],[6,7,10],
-             [7,3,9],[7,6,10],
-             [8,2,2],[8,4,7],[8,5,6]]
+    vertices = 4
+    edges = [[0,1,8],[0,3,1],
+             [1,2,1],
+             [2,3,1],
+             [3,1,2],[3,2,9]]
     noOfEdges = len(edges)
     graph(vertices, edges, noOfEdges)
