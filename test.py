ea807deed9f43f06bedee730d5f45edfe08a1ed9
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-05-12 17:35:02 +0530

    test - linear search

--- test.py
+++ test.py
@@ -1,40 +1,10 @@
-def addEdge(adj: list[list[int]], src: int, dest: int, weight: int) -> None:
-    adj[src].append([dest,weight])
+def linearSearch(arr: list[int], val: int) -> str:
+    for i in range(len(arr)):
+        if val == arr[i]:
+            return "Element is Present in the Array."
+        
+    return "Match NOT Found!"
 
-def dijkstra(adj: list[list[int]], src: int, vertices: int) -> None:
-    distances = [float('inf')] * vertices
-    distances[src] = 0
-    import heapq
-    queue = [(0, src)]
-    while queue:
-        current_weight, current_node = heapq.heappop(queue)
-        for neighbour, weight in adj[current_node]:
-            if distances[current_node] + weight < distances[neighbour]:
-                distances[neighbour] = current_weight + weight
-                heapq.heappush(queue,(distances[neighbour],neighbour))
-                print(queue)
-
-    return distances
-
-def graph(vertices: int, edges: list[list[int]], noOfEdges: int) -> None:
-    adj = [[] for _ in range(vertices)]
-    for i in range(noOfEdges):
-        addEdge(adj, edges[i][0], edges[i][1], edges[i][2])
-    # print(adj)
-    print("Shortest Distance from the node 0: ", dijkstra(adj, 0, vertices))
-
-if __name__ == "__main__" :
-    vertices = 9
-    edges = [
-        [0,1,4],[0,4,8],
-        [1,0,4],[1,2,8],[1,4,11],
-        [2,1,8],[2,3,7],[2,8,2],
-        [3,2,7],[3,6,14],[3,7,9],
-        [4,0,8],[4,1,11],[4,5,1],[4,8,7],
-        [5,4,1],[5,6,2],[5,8,6],
-        [6,2,4],[6,5,2],[6,7,10],
-        [7,3,9],[7,6,10],
-        [8,2,2],[8,4,7],[8,5,6]
-    ]
-    noOfEdges = len(edges)
-    graph(vertices, edges, noOfEdges)
+if __name__ == "__main__":
+    arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    print(linearSearch(arr,35))
