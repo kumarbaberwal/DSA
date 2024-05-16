ec3d2c5edf3ee691e400b6042d27a0b4229706b4
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-05-16 22:19:18 +0530

    test - bubble sort

--- test.py
+++ test.py
@@ -1,17 +1,12 @@
-def insertionSort(arr: list[int]) -> list[int]:
+def bubbleSort(arr: list[int]) -> list[int]:
     for i in range(len(arr)):
-        pivot = arr [i]
-        j = i - 1
-        while j >= 0 and pivot < arr [j]:
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = pivot
-
+        for j in range(len(arr)-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
-
 
 if __name__ == "__main__":
     arr = [100, 80, 90, 70, 10, 50, 20, 40, 30, 0, 60]
     arr2 = [8, 4, 9, 2, 10, 3]
-    print(insertionSort(arr))
-    print(insertionSort(arr2))
+    print(bubbleSort(arr))
+    print(bubbleSort(arr2))
