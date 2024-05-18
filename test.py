765f45b6f2a887a18936985cceff0b320a2c9907
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-05-19 00:55:55 +0530

    test - selection sort

--- test.py
+++ test.py
@@ -1,16 +1,15 @@
-def insertionSort(arr: list[int]) -> list[int]:
+def selectionSort(arr: list[int]) -> list[int]:
     for i in range(len(arr)):
-        pivot = arr[i]
-        j = i-1
-        while j>=0 and pivot < arr[j]:
-            arr[j+1] = arr[j]
-            j = j-1
-        arr[j+1] = pivot
-
+        sort = i
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[sort]:
+                sort = j
+        if arr[i] != arr[sort]:
+            arr[i], arr[sort] = arr[sort], arr[i]
     return arr
 
 if __name__ == "__main__":
     arr = [100, 80, 90, 70, 10, 50, 20, 40, 30, 0, 60]
     arr2 = [8, 4, 9, 2, 10, 3]
-    print(insertionSort(arr))
-    print(insertionSort(arr2))
+    print(selectionSort(arr))
+    print(selectionSort(arr2))
