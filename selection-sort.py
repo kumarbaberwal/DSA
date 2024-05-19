420dc0f6bafb4fd2e5a9059c58c8c10e436d2169
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-05-19 08:56:43 +0530

    selection sort

--- selection-sort.py
+++ selection-sort.py
@@ -1,10 +1,15 @@
-# Implementation of Selection sort
-a=[8,4,9,2,10,3]
-for i in range(len(a)):
-    first=i
-    for j in range(i+1,len(a)):
-        if a[j]<a[first]:
-            first=j
-    if first!=i:
-        a[first],a[i]=a[i],a[first]
-print(a)
+def selectionSort(arr: list[int]) -> list[int]:
+    for i in range(len(arr)):
+        sort = i
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[sort]:
+                sort = j
+        if arr[i] != arr[sort]:
+            arr[i], arr[sort] = arr[sort], arr[i]
+    return arr
+
+if __name__ == "__main__":
+    arr = [100, 80, 90, 70, 10, 50, 20, 40, 30, 0, 60]
+    arr2 = [8, 4, 9, 2, 10, 3]
+    print(selectionSort(arr))
+    print(selectionSort(arr2))
