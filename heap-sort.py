5ab1245a929bf6f312e62bc4b9aab8f5aa662d6f
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-05-22 01:36:56 +0530

    heap sort

--- heap-sort.py
+++ heap-sort.py
@@ -1,28 +1,30 @@
-arr=[12,11,5,6,7,1,3]
-def heapify(arr,n,i):
-    largest=i
-    l=(2*i)+1
-    r=(2*i)+2
+def HeapSort(arr: list[int]) -> list[int]:
+    for i in range((len(arr)-1//2), -1, -1):
+        Heapify(arr, len(arr), i)
 
-    if l<n and arr[l]>arr[largest]:
-        largest=l
+    for i in range(len(arr)-1, -1, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        Heapify(arr, i, 0)
 
-    if r<n and arr[r]>arr[largest]:
-        largest=r
+    return arr
 
-    if largest!=i:
-        arr[largest],arr[i]=arr[i],arr[largest]
-        heapify(arr,n,largest)
+def Heapify(arr: list[int], len: int, parent: int) -> None:
+    Largest = parent
+    left = 2 * parent + 1
+    right = 2 * parent + 2
+    if left < len and arr[left] > arr[Largest]:
+        Largest = left
+    if right < len and arr[right] > arr[Largest]:
+        Largest = right
 
+    if Largest != parent:
+        arr[parent], arr[Largest] = arr[Largest], arr[parent]
+        Heapify(arr, len, Largest)
 
-def heapsort(arr):
-    n=len(arr)-1
-    for i in range(n//2,-1,-1):
-        heapify(arr,n,i)
-
-    for i in range(n,0,-1):
-        arr[0],arr[i]=arr[i],arr[0]
-        heapify(arr,i,0)
-
-heapsort(arr)
-print(arr)
+if __name__ == "__main__":
+    arr = [100, 80, 90, 70, 10, 50, 20, 40, 30, 0, 60]
+    arr2 = [8, 4, 9, 2, 10, 3]
+    arr3 = [8, 4, 9, 2, 10, 3, 0, 10, 1, 12]
+    print(HeapSort(arr))
+    print(HeapSort(arr2))
+    print(HeapSort(arr3))
