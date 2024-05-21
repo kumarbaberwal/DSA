281ab38616d85293919ffced46af6b523d976620
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-05-22 01:36:28 +0530

    test - heap sort

--- test.py
+++ test.py
@@ -1,31 +1,30 @@
-def quickSort(arr: list[int], start: int, end: int) -> list[int]:
-    if start < end:
-        index = partition(arr, start, end)
-        quickSort(arr, start, index-1)
-        quickSort(arr, index+1, end)
+def HeapSort(arr: list[int]) -> list[int]:
+    for i in range((len(arr)-1//2), -1, -1):
+        Heapify(arr, len(arr), i)
+
+    for i in range(len(arr)-1, -1, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        Heapify(arr, i, 0)
 
     return arr
 
-def partition(arr: list[int], start: int, end: int) -> int:
-    low = start + 1
-    high = end
-    pivot = arr[start]
-    while True:
-        while low <= high and arr[low] <= pivot:
-            low += 1
-        while low <= high and arr[high] >= pivot:
-            high -= 1
-        if low <= high:
-            arr[low], arr[high] = arr[high], arr[low]
-        else:
-            break
-    arr[start], arr[high] = arr[high], arr[start]
-    return high 
+def Heapify(arr: list[int], len: int, parent: int) -> None:
+    Largest = parent
+    left = 2 * parent + 1
+    right = 2 * parent + 2
+    if left < len and arr[left] > arr[Largest]:
+        Largest = left
+    if right < len and arr[right] > arr[Largest]:
+        Largest = right
+
+    if Largest != parent:
+        arr[parent], arr[Largest] = arr[Largest], arr[parent]
+        Heapify(arr, len, Largest)
 
 if __name__ == "__main__":
     arr = [100, 80, 90, 70, 10, 50, 20, 40, 30, 0, 60]
     arr2 = [8, 4, 9, 2, 10, 3]
     arr3 = [8, 4, 9, 2, 10, 3, 0, 10, 1, 12]
-    print(quickSort(arr, 0, len(arr)-1))
-    print(quickSort(arr2, 0, len(arr2)-1))
-    print(quickSort(arr3, 0, len(arr3)-1))
+    print(HeapSort(arr))
+    print(HeapSort(arr2))
+    print(HeapSort(arr3))
