b841233074f1d0717b5433c36b52abf23025cf06
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-05-23 13:32:55 +0530

    stack

--- stack.py
+++ stack.py
@@ -1,46 +1,44 @@
 class Stack:
     def __init__(self) -> None:
-        self.stack=[]
-        self.top=len(self.stack)-1
-
-    def push(self,item):
-        self.stack.append(item)
-        self.top=self.top+1
-
-    def pop(self):
-        if self.top==-1:
-            return "underflow"
-        else :
-            item=self.stack.pop()
-            self.top=self.top-1
-            print(item)
+        self.stack = []
+        self.top = len(self.stack) - 1
     
-    def display(self):
-        if self.top==-1:
-            return "Stack is Empty!"
+    def push(self, item) -> None:
+        self.stack.append(item)
+        self.top += 1
+    def pop(self) -> None:
+        if self.top == -1:
+            return "UnderFlow!!!"
         else:
-            print(self.stack[self.top],">> Top Element")
-            # print(self.stack[self.top-1],"Top Element")
-            # print(self.stack[self.top-2],"Top Element")
-            for i in range(self.top-1,-1,-1):
+            self.top -= 1
+            return self.stack.pop()
+        
+    def display(self) -> None:
+        if self.top == -1:
+            print('Stack is Empty.')
+        else: 
+            print(self.stack[self.top], '>>> Top Element')
+            for i in range(self.top-1, -1, -1):
                 print(self.stack[i])
 
+    def isEmpty(self) -> str:
+        if self.top == -1: return True
+        else: return False
+    
 
-    def is_empty(self):
-        if self.top==-1:
-            print('True')
-        else:
-            print('False')
-
-
-stack=Stack()
-stack.push(5)
-stack.push(6)
-stack.push(9)
-stack.display()
-print() #for a empty line or next line
-stack.pop()
-print()
-stack.is_empty()
-print()
-stack.display()
+if __name__ == "__main__":
+    stack = Stack()
+    stack.push(8)
+    stack.push(9)
+    stack.push(10)
+    stack.push(2)
+    stack.push(5)
+    stack.display()
+    print(stack.isEmpty())
+    print(stack.pop())
+    print(stack.pop())
+    print(stack.pop())
+    print(stack.pop())
+    print(stack.pop())
+    stack.display()
+    print(stack.isEmpty())
