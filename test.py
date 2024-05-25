a5ac1f7a510a80dd2784e409df05680e83040677
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-05-25 19:42:50 +0530

    test - linked list

--- test.py
+++ test.py
@@ -1,45 +1,17 @@
-class Queue:
-    def __init__(self, size) -> None:
-        self.queue = []
-        self.front = self.rear = 0
-        self.size = size
+class Node:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.next = None
 
-    def enqueue(self, item) -> None:
-        if self.size == self.rear:
-            print('Queue is Full')
-        else:
-            self.queue.append(item)
-            self.rear += 1
-
-    def dequeue(self) -> None:
-        if self.front == self.rear:
-            print('Queue is Empty')
-        else:
-            self.rear -= 1
-            return self.queue.pop(0)
+class LinkedList:
+    def __init__(self) -> None:
+        self.head = None
     
-    def display(self) -> None:
-        if self.front == self.rear:
-            print('Queue is Empty')
+    def InsertAtBegining(self, data) -> None:
+        NewNode = Node(data)
+        if self.head is None:
+            self.head = NewNode
+            return
         else:
-            for i in self.queue:
-                print(i , '<--')
-
-    def isEmpty(self) -> str:
-        if self.front == self.rear: print('True')
-        else: print('False')
-
-
-if __name__ == '__main__':
-    queue = Queue(3)
-    queue.enqueue(2)
-    queue.enqueue(5)
-    queue.enqueue(7)
-    queue.enqueue(9)
-    queue.display()
-    queue.isEmpty()
-    queue.dequeue()
-    queue.dequeue()
-    queue.dequeue()
-    queue.display()
-    queue.isEmpty()
+            NewNode.next = self.head
+            self.head = NewNode
