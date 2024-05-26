900f927d33ebb109b00f9cd0120e38bf6aa67031
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-05-26 09:26:29 +0530

    test - linked list

--- test.py
+++ test.py
@@ -15,3 +15,20 @@ class LinkedList:
         else:
             NewNode.next = self.head
             self.head = NewNode
+
+    def printLL(self) -> None:
+        if self.head is None:
+            print('Linked List is Empty!')
+            return
+        else:
+            curr_none = self.head
+            while curr_none:
+                print(curr_none.data)
+                curr_none = curr_none.next
+
+if __name__ == '__main__':
+    linkedlist = LinkedList()
+    linkedlist.InsertAtBegining(9)
+    linkedlist.InsertAtBegining(10)
+    linkedlist.InsertAtBegining(11)
+    linkedlist.printLL()
