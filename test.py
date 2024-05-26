fe153f65b87f61d78490cb430df93c012e8aeefe
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-05-27 01:18:44 +0530

    test - singly linked list

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self, data) -> None:
+    def __init__(self, data: int) -> None:
         self.data = data
         self.next = None
 
@@ -7,7 +7,7 @@ class LinkedList:
     def __init__(self) -> None:
         self.head = None
     
-    def InsertAtBegining(self, data) -> None:
+    def InsertAtBegining(self, data: int) -> None:
         NewNode = Node(data)
         if self.head is None:
             self.head = NewNode
@@ -26,9 +26,22 @@ class LinkedList:
                 print(Current_Node.data)
                 Current_Node = Current_Node.next
 
+    def InsertAtIndex(self, data: int, index: int) -> None:
+        newNode = Node(data)
+        current_node = self.head
+        current_index = 0
+        while current_node and current_index != index:
+            temp = current_node
+            current_node = current_node.next
+            current_index += 1
+        temp.next = newNode
+        newNode.next = current_node
+
 if __name__ == '__main__':
     linkedlist = LinkedList()
     linkedlist.InsertAtBegining(9)
     linkedlist.InsertAtBegining(10)
     linkedlist.InsertAtBegining(11)
+    linkedlist.InsertAtIndex(12, 1)
+    linkedlist.InsertAtIndex(13, 1)
     linkedlist.printLL()
