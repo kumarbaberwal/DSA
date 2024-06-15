1ad185727340cb74fcdf428fe4b4e9a14e7ab869
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-06-15 21:54:59 +0530

    heapq

--- heap-queue.py
+++ heap-queue.py
@@ -1,42 +1,4 @@
 import heapq
-# queue=[(0,0)]
-# print(queue)
-# heapq.heappush(queue,(2,1))
-# print(queue)
-# heapq.heappush(queue,(4,2))
-# print(queue)
-# heapq.heappush(queue,(3,2))
-# print(queue)
-# heapq.heappush(queue,(9,3))
-# print(queue)
-# heapq.heappush(queue,(6,4))
-# print(queue)
-# heapq.heappush(queue,(8,3))
-# print(queue)
-# heapq.heappush(queue,(11,5))
-# print(queue)
-# heapq.heappush(queue,(9,5))
-# print(queue)
-# queue=[0]
-# print(queue)
-# heapq.heappush(queue,9)
-# print(queue)
-# heapq.heappush(queue,3)
-# print(queue)
-# heapq.heappush(queue,10)
-# print(queue)
-# heapq.heappush(queue,2)
-# print(queue)
-# heapq.heappush(queue,6)
-# print(queue)
-# heapq.heappush(queue,5)
-# print(queue)
-# heapq.heappush(queue,1)
-# print(queue)
-# heapq.heappush(queue,8)
-# print(queue)
-
-import heapq
 
 # Initial elements
 elements = [1, 3, 6, 5, 9, 8, 10, 7, 12]
