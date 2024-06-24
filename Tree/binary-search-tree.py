7cc05cd853b5dcf51955e97b3d7b126692e30fda
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-06-24 12:25:19 +0530

    binary search tree

--- Tree/binary-search-tree.py
+++ Tree/binary-search-tree.py
@@ -1,10 +1,10 @@
 class TreeNode:
-    def __init__(self,data) -> None:
-        self.data=data
-        self.left=None
-        self.right=None
+    def __init__(self, data) -> None:
+        self.data = data
+        self.left = None
+        self.right = None
 
-class BinarySearchTree:
+class BinarySeachTree:
     def __init__(self) -> None:
         self.root = None
 
@@ -12,171 +12,153 @@ class BinarySearchTree:
         if not self.root:
             self.root = TreeNode(data)
         else:
-            self.insertrecursively(self.root, TreeNode(data))
+            self._insert(self.root, data)
 
-    def insertrecursively(self, node, new_node):
-        if new_node.data < node.data:
+    def _insert(self, node, data):
+        if data < node.data:
             if not node.left:
-                node.left = new_node
+                node.left = TreeNode(data)
             else:
-                self.insertrecursively(node.left, new_node)
+                self._insert(node.left, data)
         else:
             if not node.right:
-                node.right = new_node
+                node.right = TreeNode(data)
             else:
-                self.insertrecursively(node.right, new_node)
+                self._insert(node.right, data)
 
-    def inorder(self, node):
-        root=self.root
-        if root is None:
-            print('Binary Tree is Empty.')
-            return
-        if node is not None:
-            self.inorder(node.left)
-            print(node.data)
-            self.inorder(node.right)
-
-    def preorder(self, node):
-        root=self.root  
-        if root is None:
-            print('Binary Tree is Empty!')
-            return
-        if node is not None:
-            print(node.data)
-            self.preorder(node.left)
-            self.preorder(node.right)
-
-    def postorder(self, node):
-        root=self.root
-        if root is None:
-            print('Binary Tree is Empty!')
-            return
-        if node is not None:
-            self.postorder(node.left)
-            self.postorder(node.right)
-            print(node.data)
-
-    def minimum(self):
-        node=self.root
-        if node is None:
-            print('Tree is Empty.')
+    def search(self, value):
+        if self.root:
+            self._search(self.root, value)
         else:
-            while node.left is not None:
-                node=node.left
-            print(node.data)
-
-    def minimun1(self):
-        node=self.root
-        while node!=None:
-            left=node
-            node=node.left
-        print(left.data)
-
-    def maximum(self):
-        root=self.root
-        if root is None:
-            print('Tree is Empty.')
+            print('Binary Search Tree is Empty')
+    
+    def _search(self, node, value):
+        if not node:
+            print(f'{value} is not present in the Binary Search Tree')
+        elif node.data == value:
+            print(f'{value} is Present in the Binary Search Tree')
+        elif value < node.data:
+            self._search(node.left, value)
         else:
-            while root.right is not None:
-                root=root.right
-            print(root.data)
+            self._search(node.right, value)
 
-    def maximum1(self):
-        root=self.root
-        while root!=None:
-            node=root
-            root=root.right
-        print(node.data)
-
-    def search(self,node,item):
-        if node is None:
-            print("Item is not Present in the Binary Tree.")
-            return
-        elif item==node.data:
-            print("Item Is Present IN The Binary Tree.")
-            return
-        elif item<node.data:
-            return self.search(node.left,item)
+    def delete(self, data):
+        if self.root:
+            self.root = self._delete(self.root, data)
         else:
-            return self.search(node.right,item)
-
-    def delete(self,data):
-        self._delete(self.root,data)
-
-    def _delete(self,node,data):
-        if node is None:
-            return node
-
-        if data< node.data:
-            node.left=self._delete(node.left,data)
-        elif data>node.data:
-            node.right=self._delete(node.right,data)
+            print('Binary Search Tree is Empty!')
+    
+    def _delete(self, node, data):
+        if not node:
+            return None
+        if data < node.data:
+            node.left = self._delete(node.left, data)
+        elif data > node.data:
+            node.right = self._delete(node.right, data)
         else:
-            if node.left is None:
+            if not node.left:
                 return node.right
-            elif node.right is None:
+            elif not node.right:
                 return node.left
             
-            node.data=self.min_value_node(node.right)
-            node.right=self._delete(node.right,node.data)
+            node.data = self._minValueNode(node.right)
+            node.right = self._delete(node.right, node.data)
         return node
 
-    def min_value_node(self,node):
-        current=node
-        while current.left is not None:
-            current=current.left
+    def inorderTraversal(self):
+        if self.root:
+            self._inorderTraversal(self.root)
+        else:
+            print('Binary Search Tree is Empty!')
+    
+    def _inorderTraversal(self, node):
+        if node:
+            self._inorderTraversal(node.left)
+            print(node.data)
+            self._inorderTraversal(node.right)
 
+    def preorderTraversal(self):
+        if self.root:
+            self._preorderTraversal(self.root)
+        else:
+            print('Binary Search Tree is Empty')
+
+    def _preorderTraversal(self, node):
+        if node:
+            print(node.data)
+            self._preorderTraversal(node.left)
+            self._preorderTraversal(node.right)
+    
+    def postorderTraversal(self):
+        return self._postorderTraversal(self.root) if self.root else print('Binary Search Tree is Empty')
+    
+    def _postorderTraversal(self, node):
+        if node:
+            self._postorderTraversal(node.left)
+            self._postorderTraversal(node.right)
+            print(node.data)
+
+    def findMin(self):
+        return self._minValueNode(self.root) if self.root else print('Binary Search Tree is Empty')
+    
+    def _minValueNode(self, node):
+        current = node
+        while current.left:
+            current = current.left
+        print(f'Minimum Value : {current.data}')
         return current.data
 
+    def findMax(self):
+        return self._maxValueNode(self.root) if self.root else print('Binary Search Tree is Empty')
+    
+    def _maxValueNode(self, node):
+        current = node
+        while current.right:
+            current = current.right
+        print(f'Maximum Value : {current.data}')
 
     def height(self):
-        self.length=0
-        root=self.root
-        if root is None:
-            print("Binary Tree is Empty!")
-            return
+        return print(f'The height of Binary Search Tree is: {self._height(self.root)}') if self.root else print('Binary Search Tree is Empty')
+    
+    def _height(self, node):
+        if not node:
+            return 0
         else:
-            self._height(root)
-            print('The Height of Binary Tree is : ',self.length)
+            leftHeight = self._height(node.left)
+            rightHeight = self._height(node.right)
+            return max(leftHeight, rightHeight) + 1
 
-    def _height(self,node):
-        if node is not None:
-            self._height(node.left)
-            self._height(node.right)
-            self.length+=1
-
-
-bt = BinarySearchTree()
-bt.insert(4)
-bt.insert(9)
-bt.insert(2)
-bt.insert(7)
-bt.insert(1)
-bt.insert(3)
-
-# bt.inorder(bt.root)
-# bt.preorder(bt.root)
-# bt.postorder(bt.root)
-# print()
-# print('Minimun Element: ',end=' ')
-# bt.minimum()
-# print('Maximum Element: ',end=' ')
-# bt.maximum()
-# print('Minimun Element: ',end=' ')
-# bt.minimun1()
-# print('Maximum Element: ',end=' ')
-# bt.maximum1()
-
-
-#searching items in the binary tree
-# bt.search(bt.root,1)
-
-bt.inorder(bt.root)
-print()
-bt.delete(2)
-# print(bt.delete(2))
-print()
-bt.inorder(bt.root)
-
-print()
-bt.height()
+if __name__ == "__main__":
+    bst = BinarySeachTree()
+    # bst.inorderTraversal()
+    bst.insert(9)
+    bst.insert(10)
+    bst.insert(8)
+    bst.insert(11)
+    bst.inorderTraversal()
+    print()
+    bst.search(8)
+    print()
+    bst.search(9)
+    print()
+    bst.search(10)
+    print()
+    bst.search(11)
+    print()
+    bst.search(12)
+    print()
+    bst.findMin()
+    print()
+    bst.findMax()
+    print()
+    bst.preorderTraversal()
+    print()
+    bst.postorderTraversal()
+    print()
+    bst.height()
+    print()
+    bst.delete(8)
+    bst.inorderTraversal()
+    print()
+    bst.delete(9)
+    bst.inorderTraversal()
