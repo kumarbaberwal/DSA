ece4cf61128cfa5c0a548069262b197ad05c787a
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-06-26 23:04:31 +0530

    tries

--- Tries/tries.py
+++ Tries/tries.py
@@ -1,51 +1,42 @@
 class TrieNode:
     def __init__(self) -> None:
-        self.children=[None]*26
-        self.isEndOfWord=False
-        
+        self.children = [None] * 26
+        self.isEndOfWord = False
 
 class Tries:
     def __init__(self) -> None:
-        self.root=TrieNode()
-        # print('Root',self.root)
-    
-    def _charToIndex(self,char):
-        return ord(char)-ord('a')
-    
-    def insert(self,character):
-        node=self.root
-        length=len(character)
-        for i in range(length):
-            index=ord(character[i])-ord('a')
-            if not node.children[index]:
-                node.children[index]=TrieNode()
-            node=node.children[index]
-        node.isEndOfWord=True
+        self.root = TrieNode()
 
-    def search(self,character):
-        root=self.root
-        lenght=len(character)
-        for i in range(lenght):
-            index=ord(character[i])-ord('a')   
-            if not root.children[index]:
-                return False
-            root=root.children[index]
-        return root.isEndOfWord
-
-    def display(self,character):
-        val=self.search(character)
-        if val is True:
-            print('Present in Trie.') 
+    def insert(self, word):
+        current_node = self.root
+        for i in word:
+            index = ord(i) - ord('a')
+            if not current_node.children[index]:
+                current_node.children[index] = TrieNode()
+            current_node = current_node.children[index]
+        current_node.isEndOfWord = True
+    
+    def search(self, word):
+        if self._search(word):
+            print(f'{word} is Present in Tries')
         else:
-            print('Not Present in Trie.')
+            print(f'{word} is not Present in Tries')
 
-Trie=Tries()
-characters=['arun','kumar','shivam','shreya']
-for i in characters:
-    Trie.insert(i)
+    def _search(self, word):
+        current_node = self.root
+        for i in word:
+            index = ord(i) - ord('a')
+            if not current_node.children[index]:
+                return False
+            current_node = current_node.children[index]
+        return True
+    
+if __name__ == '__main__':
+    tries = Tries()
+    Words = ['kumar', 'rohan', 'isha', 'mehak', 'rohit']
+    for i in Words:
+        tries.insert(i)
 
-print(Trie.search('kumar'))
-print(Trie.search('kunal'))
-Trie.display('kumar')
-Trie.display('shreya')
-Trie.display('kunal')
+    tries.search('kumar')
+    tries.search('mehak')
+    tries.search('isha')
