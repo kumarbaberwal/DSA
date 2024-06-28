7c382c2caeded2614322c9db8dd2e2aa11d36983
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-06-28 10:20:44 +0530

    test - bfs

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-def addEdge(adj: list[list[int]], src: int, dest: int, weight: int) -> None:
+def addEdge(adj: list[list[int]], src: int, dest: int) -> None:
 
     # Parameters:
     # adj (list[list[int]]): The adjacency list of the graph.
@@ -6,25 +6,35 @@ def addEdge(adj: list[list[int]], src: int, dest: int, weight: int) -> None:
     # dest (int): The destination vertex.
     # weight (int): The Edge weight from source to destination vertex.
 
-    adj[src].append([dest,weight])
+    adj[src].append(dest)
 
 def createGraph(vertices: int, edges: list[list[int]]) -> list[list[int]]:
     adj = [[] for _ in range(vertices)]
 
     for edge in edges:
-        addEdge(adj, edge[0], edge[1], edge[2])
+        addEdge(adj, edge[0], edge[1])
 
     return adj
 
-def printGraph(adj: list[list[int]], vertices: int) -> None:
-    for src in range(vertices):
-        print('Node', src, 'Makes an edge with')
-        for dest, weight in adj[src]:
-            print('\tNode', dest, 'with edge weight', weight)
-        print()
+def bfs(adj: list[list[int]],src: int , vertices: int) -> None:
+    visited = [False] * vertices
+    queue = [src]
+    while queue:
+        current_node = queue.pop(0)
+        if not visited[current_node]:
+            visited[current_node] = True
+            print(current_node, end='  ')
+            for neighbour in adj[current_node]:
+                queue.append(neighbour)
 
 if __name__ == "__main__":
-    vertices = 4
-    edges = [[0, 2, 2], [1, 2, 10], [1, 3, 0], [2, 0, 2], [2, 1, 10], [2, 3, -1], [3, 1, 0], [3, 2, -1]]
+    vertices = 7
+    edges = [[0, 1], [0, 2],
+             [1, 0], [1, 3],
+             [2, 0], [2, 4],
+             [3, 1], [3, 4], [3, 5],
+             [4, 2], [4, 3], [4, 5],
+             [5, 3], [5, 4], [5, 6],
+             [6, 5]]
     adjancency_list = createGraph(vertices, edges)
-    printGraph(adjancency_list, vertices)
+    bfs(adjancency_list, 0, vertices)
