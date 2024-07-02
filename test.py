8bfac8b6092af33e27e5e5c98a22af004de0dbec
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-07-03 01:41:42 +0530

    test - cycle detection undirected graph

--- test.py
+++ test.py
@@ -27,7 +27,7 @@ def createGraph(vertices: int, edges: list[list[int]]) -> list[list[int]]:
 
     return adj
 
-def isCyclicDirectedGraph(adj: list[list[int]], current_vertex: int, visited: list[bool], stack: list[bool]) -> bool:
+def isCyclicUnDirectedGraph(adj: list[list[int]], current_vertex: int, visited: list[bool], parent_vertex: int) -> bool:
     """
     Checks if there is a cycle in the directed graph using DFS.
 
@@ -35,35 +35,31 @@ def isCyclicDirectedGraph(adj: list[list[int]], current_vertex: int, visited: li
     adj (list[list[int]]): The adjacency list of the graph.
     current_vertex (int): The current vertex being explored.
     visited (list[bool]): A list indicating whether each vertex has been visited.
-    stack (list[bool]): A list indicating the recursion stack for each vertex.
+    parent_vertex (int): The parent vertex of the current vertex.
 
     Returns:
     bool: True if a cycle is detected, False otherwise.
     """
     visited[current_vertex] = True
-    stack[current_vertex] = True
     for neighbour in adj[current_vertex]:
-        if stack[neighbour]:
-            return True
-        elif not visited[neighbour]:
-            if isCyclicDirectedGraph(adj, neighbour, visited, stack):
+        if not visited[neighbour]:
+            if isCyclicUnDirectedGraph(adj, neighbour, visited, current_vertex):
                 return True
-    stack[current_vertex] = False
+        elif neighbour != parent_vertex:
+            return True
     return False
 
 if __name__ == "__main__":
-    vertices = 4
-    edges = [[0, 2], [1, 0], [2, 3], [3, 0]]
-    # edges = [[0, 1], [0, 2], [1, 3], [2, 3]]
+    vertices = 6
+    # edges = [[0, 1], [0, 4], [1, 0], [1, 2], [1, 4], [2, 1], [2, 3], [3, 2], [4, 0], [4, 1], [4, 5], [5, 4]]
+    edges = [[0, 1], [0, 4], [1, 0], [1, 2], [2, 1], [2, 3], [3, 2], [4, 0], [4, 5], [5, 4]]
     adjacency_list = createGraph(vertices, edges)
     visited = [False] * vertices
-    stack = [False] * vertices
 
-    """
-    To Reach all the Vertices of the Graph
-    """
-
+    # Check for cycles starting from each vertex
     for i in range(vertices):
         if not visited[i]:
-            print("The graph is Cyclic" if isCyclicDirectedGraph(adjacency_list, 0, visited, stack) else "The Graph is not Cyclic")
+            print("The graph is Cyclic" if isCyclicUnDirectedGraph(adjacency_list, 0, visited, -1) else "The Graph is not Cyclic")
             break
+
+    
