388e19cdd1445f0d43bffc4365c369d8affe68e8
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-07-06 14:33:29 +0530

    test - floyd warshall

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-def addEdge(adj: list[list[int]], src: int, dest: int) -> None:
+def addEdge(adj: list[list[list[int, int]]], src: int, dest: int, weight: int) -> None:
 
     """Parameters:
     adj (list[list[int]]): The adjacency list of the graph.
@@ -7,7 +7,7 @@ def addEdge(adj: list[list[int]], src: int, dest: int) -> None:
     weight(int): The Weight from source to destination
     """
 
-    adj[src].append(dest)
+    adj[src].append([dest, weight])
 
 def createGraph(vertices: int, edges: list[list[int]]) -> list[list[int]]:
 
@@ -25,71 +25,50 @@ def createGraph(vertices: int, edges: list[list[int]]) -> list[list[int]]:
     adj = [[] for _ in range(vertices)]
 
     for edge in edges:
-        addEdge(adj, edge[0], edge[1])
+        addEdge(adj, edge[0], edge[1], edge[2])
 
     return adj
 
-def dfs(adj: list[list[int]], current_vertex: int, visited: list[bool], dt: list[int], lt: list[int], time: list[int], parent_vertex: int, articulationPoint: list[bool]) -> None:
+def floydWarshall(adj: list[list[list[int, int]]], vertices: int) -> list[list[int]]:
     """
-    Depth-first search to find and print all articulation points in the graph.
+    Implements the Floyd-Warshall algorithm to find the shortest paths between all pairs of vertices.
 
     Parameters:
-    adj (List[List[int]]): The adjacency list of the graph.
-    current_vertex (int): The current vertex being explored.
-    parent_vertex (int): The parent vertex of the current vertex in the DFS tree.
-    dt (List[int]): Discovery times of visited vertices.
-    lt (List[int]): Lowest discovery times reachable from the vertex.
-    visited (List[bool]): A list indicating whether each vertex has been visited.
-    time (List[int]): Current time counter.
-    articulationPoint (List[bool]): A list indicating whether each vertex is an articulation point.
-    """
-    visited[current_vertex] = True
-    dt[current_vertex] = lt[current_vertex] = time[0]
-    time[0] += 1
-    children = 0
-
-    for neighbour in adj[current_vertex]:
-        if neighbour == parent_vertex:
-            continue
-        if not visited[neighbour]:
-            dfs(adj, neighbour, visited, dt, lt, time, current_vertex, articulationPoint)
-            lt[current_vertex] = min(lt[current_vertex], lt[neighbour])
-
-            if parent_vertex != -1 and dt[current_vertex] <= lt[neighbour]:
-                articulationPoint[current_vertex] = True
-
-            children += 1
-        else:
-            lt[current_vertex] = min(lt[current_vertex], dt[neighbour])
-
-    if parent_vertex == -1 and children > 1:
-        articulationPoint[current_vertex] = True
-
-def getArticulationPoint(adj: list[list[int]], vertices: int) -> None:
-    """
-    Finds and prints all articulation points in the graph.
-
-    Parameters:
-    adj (List[List[int]]): The adjacency list of the graph.
+    adj (List[List[List[int]]]): The adjacency list of the graph.
     vertices (int): The number of vertices in the graph.
+
+    Returns:
+    List[List[int]]: A 2D list representing the shortest distances between every pair of vertices.
     """
-    dt = [-1] * vertices
-    lt = [-1] * vertices
-    time = [0]
-    visited = [False] * vertices
-    articulationPoint = [False] * vertices
+    distance = [[float('inf') for _ in range(vertices)] for _ in range(vertices)]
+    
+    # Set initial distances based on direct edges
+    for i in range(vertices):
+        for j, weight in adj[i]:
+            distance[i][j] = weight
+    
+    # Set distance to self as 0
+    for i in range(vertices):
+        distance[i][i] = 0
 
-    for source in range(vertices):
-        if not visited[source]:
-            dfs(adj, source, visited, dt, lt, time, -1, articulationPoint)
+    # Update distances using Floyd-Warshall algorithm
+    for k in range(vertices):
+        for i in range(vertices):
+            for j in range(vertices):
+                distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j])
 
-    for vertex in range(vertices):
-        if articulationPoint[vertex]:
-            print('Articulation Point:', vertex)
+    return distance
 
 if __name__ == "__main__":
-    vertices = 5
-    edges = [[0, 1], [0, 2], [0, 3], [1, 0], [1, 2], [2, 0], [2, 1], [3, 0], [3, 4], [4, 3]]
+    vertices = 4
+    edges = [[0,1,8],[0,3,1],
+             [1,2,1],
+             [2,0,4],
+             [3,1,2],[3,2,9]]
     adjacency_list = createGraph(vertices, edges)
-    getArticulationPoint(adjacency_list, vertices)
+    distances = floydWarshall(adjacency_list, vertices)
+    
+    print('The shortest distances between every pair of vertices:')
+    for row in distances:
+        print(row)
     
