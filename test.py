cd3b52d914a06c20f5edfb4f90264327bf341f10
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-07-06 14:23:09 +0530

    test - getArticulationPoint

--- test.py
+++ test.py
@@ -29,33 +29,67 @@ def createGraph(vertices: int, edges: list[list[int]]) -> list[list[int]]:
 
     return adj
 
-def topologicalSort(adj: list[list], current_vertex: int, visited: list[bool], stack: list[int]):
+def dfs(adj: list[list[int]], current_vertex: int, visited: list[bool], dt: list[int], lt: list[int], time: list[int], parent_vertex: int, articulationPoint: list[bool]) -> None:
     """
-    Performs a depth-first search (DFS) based topological sort on the graph.
+    Depth-first search to find and print all articulation points in the graph.
 
     Parameters:
     adj (List[List[int]]): The adjacency list of the graph.
     current_vertex (int): The current vertex being explored.
+    parent_vertex (int): The parent vertex of the current vertex in the DFS tree.
+    dt (List[int]): Discovery times of visited vertices.
+    lt (List[int]): Lowest discovery times reachable from the vertex.
     visited (List[bool]): A list indicating whether each vertex has been visited.
-    stack (List[int]): A stack to store the topologically sorted vertices.
+    time (List[int]): Current time counter.
+    articulationPoint (List[bool]): A list indicating whether each vertex is an articulation point.
     """
     visited[current_vertex] = True
+    dt[current_vertex] = lt[current_vertex] = time[0]
+    time[0] += 1
+    children = 0
+
     for neighbour in adj[current_vertex]:
+        if neighbour == parent_vertex:
+            continue
         if not visited[neighbour]:
-            topologicalSort(adj, neighbour, visited, stack)
+            dfs(adj, neighbour, visited, dt, lt, time, current_vertex, articulationPoint)
+            lt[current_vertex] = min(lt[current_vertex], lt[neighbour])
 
-    stack.append(current_vertex)
+            if parent_vertex != -1 and dt[current_vertex] <= lt[neighbour]:
+                articulationPoint[current_vertex] = True
 
-if __name__ == "__main__":
-    vertices = 6
-    edges = [[2, 3] ,[3 ,1] ,[4 ,0] ,[4 ,1] ,[5 ,0] ,[5, 2]]
-    adjacency_list = createGraph(vertices, edges)
+            children += 1
+        else:
+            lt[current_vertex] = min(lt[current_vertex], dt[neighbour])
+
+    if parent_vertex == -1 and children > 1:
+        articulationPoint[current_vertex] = True
+
+def getArticulationPoint(adj: list[list[int]], vertices: int) -> None:
+    """
+    Finds and prints all articulation points in the graph.
+
+    Parameters:
+    adj (List[List[int]]): The adjacency list of the graph.
+    vertices (int): The number of vertices in the graph.
+    """
+    dt = [-1] * vertices
+    lt = [-1] * vertices
+    time = [0]
     visited = [False] * vertices
-    stack = []
+    articulationPoint = [False] * vertices
+
     for source in range(vertices):
         if not visited[source]:
-            topologicalSort(adjacency_list, source, visited, stack)
+            dfs(adj, source, visited, dt, lt, time, -1, articulationPoint)
 
-    print("Topological Sort of the given graph:")
-    while stack:
-        print(stack.pop(), end=' ')
+    for vertex in range(vertices):
+        if articulationPoint[vertex]:
+            print('Articulation Point:', vertex)
+
+if __name__ == "__main__":
+    vertices = 5
+    edges = [[0, 1], [0, 2], [0, 3], [1, 0], [1, 2], [2, 0], [2, 1], [3, 0], [3, 4], [4, 3]]
+    adjacency_list = createGraph(vertices, edges)
+    getArticulationPoint(adjacency_list, vertices)
+    
